22b9afd41081248536004ce0cbab4cfd15111fad
me.luc <lukasalmeida372@gmail.com>
2023-05-18 19:05:38 -0300

    feat: update api key context

--- src/context/ApiKeyContext.tsx
+++ src/context/ApiKeyContext.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { createContext, useState } from 'react';
 
 interface ApiKeyContent {
 	apiKey: string;
+	setApiKey: (apiKey: string) => void;
 }
 
 const ApiKeyContext = createContext<ApiKeyContent>({
 	apiKey: '',
+	setApiKey: () => {},
 });
 export default ApiKeyContext;
 
@@ -16,9 +19,19 @@ interface ApiKeyProviderProps {
 export function ApiKeyProvider({ children }: ApiKeyProviderProps) {
 	const [apiKey, setApiKey] = useState('');
 
+	const savedApiKey = loadApiKey();
+	if (savedApiKey) {
+		setApiKey(savedApiKey);
+	}
+
 	return (
-		<ApiKeyContext.Provider value={{ apiKey }}>
+		<ApiKeyContext.Provider value={{ apiKey, setApiKey }}>
 			{children}
 		</ApiKeyContext.Provider>
 	);
+}
+
+function loadApiKey() {
+	const apiKey = localStorage.getItem('apiKey');
+	return apiKey;
 }
