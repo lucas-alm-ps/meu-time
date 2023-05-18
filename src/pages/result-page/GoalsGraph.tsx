4c9cfb309d683818f82ea58957dc517f2868f76d
me.luc <lukasalmeida372@gmail.com>
2023-05-17 21:39:25 -0300

    build: react google charts

--- src/pages/result-page/GoalsGraph.tsx
+++ src/pages/result-page/GoalsGraph.tsx
@@ -1,33 +1,3 @@
 export default function GoalsGraph() {
-	const dataMockup = {
-		minute: {
-			'0-15': {
-				total: 4,
-				percentage: '6.06%',
-			},
-			'16-30': {
-				total: 17,
-				percentage: '25.76%',
-			},
-			'31-45': {
-				total: 11,
-				percentage: '16.67%',
-			},
-		},
-	};
-
-	const minuteData = Object.entries(dataMockup.minute).map(
-		([minuteRange, range]) => ({
-			name: minuteRange,
-			percentage: percentageStrToNumber(range.percentage),
-		})
-	);
-
-	console.log(minuteData);
-
 	return <></>;
-}
-
-function percentageStrToNumber(str: string) {
-	return Number(str.replace('%', ''));
 }
